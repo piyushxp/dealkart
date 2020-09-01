65604ce928deda7a0c833b02dda1a4cc71ffa33a
Piyush Mahapatra <supertramp_piyush@pop-os.localdomain>
2020-09-01 22:12:47 +0530

    admin

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
-const {signup,signin,requireSignin} = require('../controllers/authController.js')
+const {signup,signin,requireSignin} = require('../controllers/adminController.js')
 
 const router = express.Router()
 
 
-// router.route('/admin/signup').post(signup)
-// router.route('/admin/signin').post(signin)
+router.route('/admin/signup').post(signup)
+router.route('/admin/signin').post(signin)
 
 
 module.exports = router
