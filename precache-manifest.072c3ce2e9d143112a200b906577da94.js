c70bef20e35fd2f0ce4d731e0b87aad248c37d87
Piyush Mahapatra <supertramp_piyush@pop-os.localdomain>
2020-09-04 18:53:53 +0530

    Updates

---
+++ precache-manifest.072c3ce2e9d143112a200b906577da94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb6a7b3d0d6095ced0ca27f60fbe4b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dd6c1bc0b6bd301e1c3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3488c45d4a70e324f077",
+    "url": "/static/js/2.130efef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.130efef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd6c1bc0b6bd301e1c3",
+    "url": "/static/js/main.462531b4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
