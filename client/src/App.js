73a565faa0236fa3633ddd5b00b32a063e371574
Piyush Mahapatra <supertramp_piyush@pop-os.localdomain>
2020-09-04 18:51:13 +0530

    Work in progress

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ComingSoon from "react-coming-soon";
+
+export default function App() {
+	return (
+		<>
+			<ComingSoon
+				image='https://react-coming-soon.maksv.me/default-image.jpeg'
+				bgColor='#fff'
+				textColor='#000'
+				illustration='development'
+			/>
+		</>
+	);
+}
