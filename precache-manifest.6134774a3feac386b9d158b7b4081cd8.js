f8f3d5e5c13ae64b59982c3985089aa6f79153a4
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-09-05 17:16:28 +0530

    Updates

---
+++ precache-manifest.6134774a3feac386b9d158b7b4081cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1165baa6c01107e9c764872171db3829",
+    "url": "/dealkart/index.html"
+  },
+  {
+    "revision": "eff306cabad551847d9a",
+    "url": "/dealkart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3488c45d4a70e324f077",
+    "url": "/dealkart/static/js/2.130efef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dealkart/static/js/2.130efef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff306cabad551847d9a",
+    "url": "/dealkart/static/js/main.61752235.chunk.js"
+  },
+  {
+    "revision": "3c11d71509cacc739c52",
+    "url": "/dealkart/static/js/runtime-main.d0e4fc56.js"
+  }
+]);
